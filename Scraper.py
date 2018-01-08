5e9c78ac8de7b74a771b5c9c31612e6c9019a22a
MartaKapielska <marta.kapielska@gmail.com>
2018-01-08 23:11:06 +0100

    finished functionality, but lacks refactorization

--- Scraper.py
+++ Scraper.py
@@ -1,8 +1,9 @@
+import io
 from bs4 import BeautifulSoup
 import requests
 
 imdb_start_page = 'http://www.imdb.com'
-storyline_directory = "/movies_txt_storylines"
+storyline_directory = "movies_txt_storylines/"
 movie_count_limit = 25
 comedy_link = "http://www.imdb.com/search/title?genres=comedy&explore=title_type,genres&pf_rd_m=A2FGELUUNOQJNL&pf_rd_" \
               "p=3454807202&pf_rd_r=0T1FF6HQPTKMMRRV89T5&pf_rd_s=center-1&pf_rd_t=15051&pf_rd_i=genre&sort=num_votes," \
@@ -16,25 +17,43 @@ action_link = "http://www.imdb.com/search/title?genres=action&explore=title_type
 fantasy_link = "http://www.imdb.com/search/title?genres=fantasy&explore=title_type,genres&pf_rd_m=A2FGELUUNOQJNL&pf_" \
                "rd_p=3454807302&pf_rd_r=0MVVH2XNR401HJZ01YJ1&pf_rd_s=center-4&pf_rd_t=15051&pf_rd_i=genre&sort=num_" \
                "votes,desc&ref_=ft_gnr_pr4_i_3"
+html_link_selector = 'div.lister-item h3.lister-item-header a'
+link_pollution = "?ref_=adv_li_tt"
+html_sypnosis_selector = "li[id*=synopsis]"
 
 
 def get_movies_for_genre(link):
     response = requests.get(link)
     soup = BeautifulSoup(response.text, 'html.parser')
-    movie_links = [a.attrs.get('href') for a in soup.select('div.lister-item h3.lister-item-header a')]
+    movie_links = [a.attrs.get('href') for a in soup.select(html_link_selector)]
     return movie_links[:movie_count_limit]
 
 
 def clean_movie_link(link):
-    return link.replace("?ref_=adv_li_tt", "")
+    return link.replace(link_pollution, "")
 
 
 def get_movie_storyline(link):
-    response = requests.get(imdb_start_page + clean_movie_link(link) + "plotsummary")
+    response = requests.get(generate_movie_link(link))
     soup = BeautifulSoup(response.text, 'html.parser')
-    return soup.select_one("li[id*=synopsis]").get_text()
+    return soup.select_one(html_sypnosis_selector).get_text()
 
 
+def generate_movie_link(link):
+    return imdb_start_page + clean_movie_link(link) + "plotsummary"
+
+
+# TODO: wywalic generate na zewnatrz
+def save_to_file(text, f_name_prefix, number):
+    with io.open(generate_file_name(f_name_prefix, number), "w", encoding="utf-8") as f:
+        f.write(text)
+
+
+def generate_file_name(f_name_prefix, number):
+    return storyline_directory + f_name_prefix + str(number) + '.txt'
+
+
+# TODO: refactor
 print(get_movies_for_genre(romance_link))
-print(get_movie_storyline(get_movies_for_genre(fantasy_link)[1]))
+save_to_file(get_movie_storyline(get_movies_for_genre(fantasy_link)[1]), 'fantasy', 0)
 
